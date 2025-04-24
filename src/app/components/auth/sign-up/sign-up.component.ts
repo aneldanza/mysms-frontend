3a2b5a4c836b7583d39cd82b252964e84a85a6f1
Anel Danza <aneldanza23@gmail.com>
2025-04-24 16:42:05 -0400

    create auth components

---
+++ src/app/components/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth/auth.service';
+import {
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-sign-up',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css',
+})
+export class SignUpComponent {
+  form: FormGroup;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      username: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.auth.signUp(this.form.value).subscribe({
+        next: () => {
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+    }
+  }
+}
