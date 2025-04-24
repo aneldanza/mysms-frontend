2c6af1e7c963278b96af123086aa964faea89da3
Anel Danza <aneldanza23@gmail.com>
2025-04-24 18:54:42 -0400

    create auth guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+
+  return auth.isLoggedIn()
+    ? true
+    : router.navigate(['/sign-in'], { queryParams: { redirect: state.url } });
+};
