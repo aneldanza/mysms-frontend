f403d129064d6021f4ad952ddcc715418f98b086
Anel Danza <aneldanza23@gmail.com>
2025-04-23 22:01:20 -0400

    create MessageFormComponent with sendMessage method and state

--- src/app/components/message-form/message-form.component.ts
+++ src/app/components/message-form/message-form.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Message, MessagesService } from '../../services/messages.service';
 
 @Component({
   selector: 'app-message-form',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './message-form.component.html',
-  styleUrl: './message-form.component.css'
+  styleUrl: './message-form.component.css',
 })
 export class MessageFormComponent {
+  message: Message = {
+    to: '+18777804236',
+    body: '',
+  };
 
+  status: string = '';
+  loading = false;
+
+  constructor(private messagesService: MessagesService) {}
+
+  sendMessage() {
+    this.loading = true;
+    this.status = '';
+
+    this.messagesService.sendMessage(this.message).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.status = 'Message sent successfully!';
+        this.message = { to: '', body: '' };
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.status = 'Failed to send message.';
+        this.loading = false;
+      },
+    });
+  }
 }
