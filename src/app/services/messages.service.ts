ef40aba0d54f3e269f1a95b41fe9607ec39378ee
Anel Danza <aneldanza23@gmail.com>
2025-04-23 21:01:05 -0400

    create MessagesService, add sendMessage and getMessages methods

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Message {
+  _id?: string;
+  to: string;
+  body: string;
+  status?: string;
+  createdAt?: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessagesService {
+  private apiUrl = 'http://localhost:3000/messages';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  sendMessage(message: Message): Observable<Message> {
+    return this.http.post<Message>(this.apiUrl, { message });
+  }
+
+  getMessages(): Observable<Message[]> {
+    return this.http.get<Message[]>(this.apiUrl);
+  }
 }
