cb79c6c18073fe0cc0e2b1fad5e99cd470771dd2
Anel Danza <aneldanza23@gmail.com>
2025-04-24 18:04:19 -0400

    add routes for sign-in and sign-out components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { MessageFormComponent } from './components/message-form/message-form.component';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MessageFormComponent,
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
   },
 ];
