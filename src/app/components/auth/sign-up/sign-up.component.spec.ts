1498fca42ec69b3410f0a1135685d511c8accc4b
Anel Danza <aneldanza23@gmail.com>
2025-04-25 01:08:44 -0400

    clean up

--- src/app/components/auth/sign-up/sign-up.component.spec.ts
+++ src/app/components/auth/sign-up/sign-up.component.spec.ts
@@ -1,28 +1,3 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { provideHttpClient } from '@angular/common/http';
-// import { SignUpComponent } from './sign-up.component';
-// import { provideRouter } from '@angular/router';
-
-// describe('SignUpComponent', () => {
-//   let component: SignUpComponent;
-//   let fixture: ComponentFixture<SignUpComponent>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [SignUpComponent],
-//       providers: [provideHttpClient(), provideRouter([])], // Provide router with empty routes
-//     }).compileComponents();
-
-//     fixture = TestBed.createComponent(SignUpComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SignUpComponent } from './sign-up.component';
 import { ReactiveFormsModule } from '@angular/forms';
