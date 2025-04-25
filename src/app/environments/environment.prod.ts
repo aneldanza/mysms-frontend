3ac85a82450836ab02a0f35c7675e57ad62d86cd
Anel Danza <aneldanza23@gmail.com>
2025-04-25 07:35:02 -0400

    use ws://

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseApi: 'https://sms-messenger-api-9c3caa66eb6a.herokuapp.com',
-  cableServiceUrl: 'wss://sms-messenger-api-9c3caa66eb6a.herokuapp.com/cable',
+  cableServiceUrl: 'ws://sms-messenger-api-9c3caa66eb6a.herokuapp.com/cable',
   tokenKey: 'sms_messenger_jwt',
   toPhoneNumber: '+18777804236',
 };
