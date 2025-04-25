6b6bd7a673c643765e3ae2613fb52aac3b691c44
Anel Danza <aneldanza23@gmail.com>
2025-04-25 17:19:20 -0400

    more styling

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  // baseApi: 'https://mysms-api.onrender.com',
-  // cableServiceUrl: 'wss://mysms-api.onrender.com/cable',
-  baseApi: 'http://localhost:3000',
-  cableServiceUrl: 'ws://localhost:3000/cable',
+  baseApi: 'https://mysms-api.onrender.com',
+  cableServiceUrl: 'wss://mysms-api.onrender.com/cable',
+  // baseApi: 'http://localhost:3000',
+  // cableServiceUrl: 'ws://localhost:3000/cable',
   tokenKey: 'sms_messenger_jwt',
   toPhoneNumber: '+18777804236',
 };
