87973bc3a39af11025976f65fd053b110647960a
Anel Danza <aneldanza23@gmail.com>
2025-04-24 21:48:20 -0400

    create message home component

---
+++ src/app/components/message-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-home',
+  imports: [],
+  templateUrl: './message-home.component.html',
+  styleUrl: './message-home.component.css'
+})
+export class MessageHomeComponent {
+
+}
