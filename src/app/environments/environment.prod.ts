ef4dd32f70c2d75bb3fe70e96835f7fd8a938540
Anel Danza <aneldanza23@gmail.com>
2025-04-25 00:16:06 -0400

    add to phone number to env variables

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   baseApi: 'https://api.example.com',
   tokenKey: 'sms_messenger_jwt',
+  toPhoneNumber: '+18777804236',
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   baseApi: 'http://localhost:3000',
   tokenKey: 'sms_messenger_jwt',
+  toPhoneNumber: '+18777804236',
 };
