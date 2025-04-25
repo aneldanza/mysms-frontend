4810f8ec7343ca539d3399a10d4c0ba12fc43535
Anel Danza <aneldanza23@gmail.com>
2025-04-25 01:08:17 -0400

    update message list tests

--- src/app/components/message-list/message-list.component.spec.ts
+++ src/app/components/message-list/message-list.component.spec.ts
@@ -1,27 +1,3 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { provideHttpClient } from '@angular/common/http';
-// import { MessageListComponent } from './message-list.component';
-
-// describe('MessageListComponent', () => {
-//   let component: MessageListComponent;
-//   let fixture: ComponentFixture<MessageListComponent>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [MessageListComponent],
-//       providers: [provideHttpClient()],
-//     }).compileComponents();
-
-//     fixture = TestBed.createComponent(MessageListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MessageListComponent } from './message-list.component';
 import { MessagesService } from '../../services/messages/messages.service';
